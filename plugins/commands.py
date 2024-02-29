d63a46e7123c37a8d7ab93265147605a4aa1af08
Jkdeveloper01 <142569922+Jkdeveloper01@users.noreply.github.com>
2024-02-29 21:20:25 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -432,6 +432,7 @@ async def start(client, message):
     await asyncio.sleep(600)
     await msg.delete()
     await dev.edit_text("<b>ʏᴏᴜʀ ᴠɪᴅᴇᴏ / ꜰɪʟᴇ ɪꜱ ꜱᴜᴄᴄᴇꜱꜱꜰᴜʟʟʏ ᴅᴇʟᴇᴛᴇᴅ !!\n\nᴄʟɪᴄᴋ ʙᴇʟᴏᴡ ʙᴜᴛᴛᴏɴ ᴛᴏ ɢᴇᴛ ʏᴏᴜʀ ᴅᴇʟᴇᴛᴇᴅ ᴠɪᴅᴇᴏ / ꜰɪʟᴇ 👇</b>",reply_markup=InlineKeyboardMarkup(btn))
+    return 
     
 @Client.on_message(filters.command('channel') & filters.user(ADMINS))
 async def channel_info(bot, message):
