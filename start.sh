c7c90789c45917015580ffe58ff351bee66e0379
The Happy Hour <159997953+The-Happy-Hour@users.noreply.github.com>
2024-03-27 10:06:32 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/MasterJiraya005/JK-2ed-Bot /JK-2ed-Bot
+  git clone https://github.com/The-Happy-Hour/New-Premium-Bot /New-Premium-Bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /JK-2ed-Bot
+  git clone $UPSTREAM_REPO /New-Premium-Bot
 fi
-cd /JK-2ed-Bot
+cd /New-Premium-Bot
 pip3 install -U -r requirements.txt
 echo "Starting DQ-The-File-Donor...."
 python3 bot.py
