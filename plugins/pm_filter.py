3d757ee2179a394a1166cccb138bf281ef3bae56
THH <159997953+The-Happy-Hour@users.noreply.github.com>
2024-05-02 21:38:04 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -96,7 +96,7 @@ async def stream_download(bot, query):
                 ]
             ))
 
-@Client.on_message(filters.group | filters.private & filters.text & filters.incoming)
+@Client.on_message(filters.group & filters.text & filters.incoming)
 async def give_filter(client, message):
     if message.chat.id != SUPPORT_CHAT_ID:
         glob = await global_filters(client, message)
@@ -146,7 +146,7 @@ async def pm_text(bot, message):
     user_id = message.from_user.id
     if content.startswith("/") or content.startswith("#"): return  # ignore commands and hashtags
     if user_id in ADMINS: return # ignore admins
-    await message.reply_text("<b>Type & Get - t.me/Happy_Hour_Friends</b>")
+    await message.reply_text("<b>Request Hare - t.me/ThappyHour\nRequest Hare - t.me/ThappyHour\nRequest Hare - t.me/ThappyHour</b>")
     await bot.send_message(
         chat_id=LOG_CHANNEL,
         text=f"<b>#𝐏𝐌_𝐌𝐒𝐆\n\nNᴀᴍᴇ : {user}\n\nID : {user_id}\n\nMᴇssᴀɢᴇ : {content}</b>"
