b850509b3dbe53eee0062097de371df3f591513a
shiv9969 <150778501+shiv9969@users.noreply.github.com>
2024-07-04 12:33:15 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,1 @@
-FROM python:3.10.8-slim-buster
-COPY requirements.txt .
-RUN apt update -y && apt install git -y
-RUN pip3 install -U -r requirements.txt
-COPY . .
-CMD ["/bin/bash", "/start.sh"]
-echo "Starting DQ-The-File-Donor...."
 python3 bot.py
