84bf7241f9a31493d32a40866b292c824d4c7ed2
shiv9969 <150778501+shiv9969@users.noreply.github.com>
2024-07-04 12:08:54 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,12 @@
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/shiv9969/Itachi-premium-bot- /New-Premium-Bot
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /New-Premium-Bot
+fi
+cd /New-Premium-Bot
 pip3 install -U -r requirements.txt
 echo "Starting DQ-The-File-Donor...."
 python3 bot.py
